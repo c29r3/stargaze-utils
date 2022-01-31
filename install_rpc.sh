1d87d2b4b5c816cf1d3ce73f44a95b6c19cd23fd
Sergey <52304443+c29r3@users.noreply.github.com>
2022-01-31 04:36:36 +0300

    Update install_rpc.sh

--- install_rpc.sh
+++ install_rpc.sh
@@ -13,7 +13,7 @@ PORT_OFFSET=1
 
 # install requirements
 sudo apt-get update
-sudo apt-get install -y jq git curl wget make bc
+sudo apt-get install -y jq git curl wget build-essential bc
 curl -s https://gist.githubusercontent.com/c29r3/3130b5cd51c4a94f897cc58443890c28/raw/134d86f8a90b2bbb7c68cd6bb663c60c5846ae31/install_golang.sh | bash -s - 1.17.1
 
 # install binary
