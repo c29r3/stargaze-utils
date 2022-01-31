63db41cc8355b152fd8616f428672ad0f0130aa7
Sergey <52304443+c29r3@users.noreply.github.com>
2022-01-31 03:47:07 +0300

    pzdc

--- install_rpc.sh
+++ install_rpc.sh
@@ -3,13 +3,18 @@
 CONF=$(curl -s https://raw.githubusercontent.com/c29r3/stargaze-utils/main/stargaze.json)
 BINARY_LINK=$(jq -r .binary_link <<< $CONF)
 BIN_NAME=$(jq -r .binary_name <<< $CONF)
-CHAIN_ID=$(jq -r .chain-id <<< $CONF)
+CHAIN_ID=$(jq -r .chain_id <<< $CONF)
 COIN=$(jq -r .coin <<< $CONF)
 GENESIS=$(jq -r .genesis <<< $CONF)
 SEEDS=$(jq -r .seeds <<< $CONF)
 PEERS=$(jq -r .persistent_peers <<< $CONF)
 RPC_SERVERS=$(jq -r .rpc_servers <<< $CONF)
 PORT_OFFSET=1
+
+# install requirements
+sudo apt-get update
+sudo apt-get install -y jq git curl wget make bc
+curl -s https://gist.githubusercontent.com/c29r3/3130b5cd51c4a94f897cc58443890c28/raw/134d86f8a90b2bbb7c68cd6bb663c60c5846ae31/install_golang.sh | bash -s - 1.17.1
 
 # install binary
 wget -qO- $BINARY_LINK | tar -C /usr/local/bin/ -xzf-
@@ -18,7 +23,7 @@ wget -qO- $BINARY_LINK | tar -C /usr/local/bin/ -xzf-
 $BIN_NAME version
 
 # install service file
-echo '[Unit]
+echo -e "[Unit]
 Description=Stargaze Daemon
 After=network-online.target
 
@@ -30,7 +35,7 @@ RestartSec=3
 LimitNOFILE=65535
 
 [Install]
-WantedBy=multi-user.target' > /etc/systemd/system/$BIN_NAME.service
+WantedBy=multi-user.target" > /etc/systemd/system/$BIN_NAME.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable $BIN_NAME
@@ -63,15 +68,17 @@ sed -i "s|address = \"tcp://0.0.0.0:1317\"|address = \"tcp://0.0.0.0:1"$PORT_OFF
 
 sed -i "s|address = \":8080\"|address = \":8"$PORT_OFFSET"80\"|g" $APP_CONF_PATH
 
-sed -i "s|enable = "'false'"|enable = "'true'"|g" $APP_CONF_PATH
+# sed -i "s|enable = "'false'"|enable = "'true'"|g" $APP_CONF_PATH
+sed -i "s|swagger = "'false'"|swagger = "'true'"|g" $APP_CONF_PATH
 
 sed -i "s|snapshot-interval = 0|snapshot-interval = 500|g" $APP_CONF_PATH
 
 # configure statesync if rpc_list is not empty
 if [ $RPC_SERVERS != "" ]; then
-	statesync_data=$(curl -s echo "$(echo $RPC_SERVERS | cut -d ',' -f 1)/commit" | jq "{height: .result.signed_header.header.height, hash: .result.signed_header.commit.block_id.hash}")
-	height=$(jq -r .height <<< $statesync_data)
-	hash=$(jq -r .hash <<< $statesync_data)
+	first_rpc=$(echo $RPC_SERVERS | cut -d ',' -f 1)
+	latest_block=$(curl -s echo "$first_rpc/status" | jq -r .result.sync_info.latest_block_height)
+	height=$(echo "$latest_block-1000" | bc)
+	hash=$(curl -s "$first_rpc/block?height=$height" | jq -r .result.block_id.hash)
 
 	sed -i "s|enable = "'false'"|enable = "'true'"|g" $CONF_PATH
 	sed -i "s|rpc_servers = \"\"|rpc_servers = \"$RPC_SERVERS\"|g" $CONF_PATH
@@ -79,6 +86,13 @@ if [ $RPC_SERVERS != "" ]; then
 	sed -i "s|trust_hash = \"\"|trust_hash = \"$hash\"|g" $CONF_PATH
 
 fi
+
+cd ~
+git clone https://github.com/tendermint/tendermint
+cd tendermint
+git checkout callum/app-version
+make install
+~/go/bin/tendermint set-app-version 1 --home ~/.$BIN_NAME
 
 sudo systemctl start $BIN_NAME
 sudo systemctl status $BIN_NAME
